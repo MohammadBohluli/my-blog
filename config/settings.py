5aa5cbda162b3ed2b22f9f56be2e950d2f7182e6
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2022-08-26 13:42:01 +0430

    create app accounts

--- config/settings.py
+++ config/settings.py
@@ -31,13 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # Django Applications
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My Applications
+    'accounts.apps.AccountsConfig'
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUserModel'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
