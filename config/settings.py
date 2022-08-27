453773a94bd66daf8407f468a23ee7fd594027eb
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2022-08-27 12:42:43 +0430

    feat: add blog app and create template structure

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # My Applications
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'blog.apps.BlogConfig'
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomUserModel'
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
