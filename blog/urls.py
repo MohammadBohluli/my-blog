854416f3fa0e1987bd29f21ef275e76d397c2817
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2022-09-24 19:22:33 +0330

    feature:add bootstrap 5 to the projects

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import home_page_view, about_page_view
 
+app_name = "blog"
 urlpatterns = [
     path('', home_page_view, name='home_page'),
     path('about/', about_page_view, name='about_page')
