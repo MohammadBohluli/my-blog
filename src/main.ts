a1e795fb9108c49ce2858edce807f4e9a7f6f55f
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-10-10 00:59:36 +0330

    initialize swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('My Blog API')
+    .setDescription('This project is a personal blog api')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
