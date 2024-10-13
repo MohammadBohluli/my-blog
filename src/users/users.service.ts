1629a31e79c274e629e627a1f584feaf99b117f8
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-10-13 19:25:39 +0330

    implement hash password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
     const { email, username, name, password } = createUserDto;
-    return this.prisma.users.create({
-      data: { email, username, password, name },
+
+    const hashPassword = await argon2.hash(password);
+
+    const user = await this.prisma.users.create({
+      data: { email, username, password: hashPassword, name },
     });
+
+    return user;
   }
 
   findByUsername(username: string) {
