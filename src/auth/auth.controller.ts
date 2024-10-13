a43df86c08bb9c229d23495b91c89157d3d908f0
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-10-13 17:56:53 +0330

    implement signup controller auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly userService: UsersService) {}
+  @Post('signup')
+  signup(@Body() body: CreateUserDto) {
+    return this.userService.create(body);
+  }
+}
