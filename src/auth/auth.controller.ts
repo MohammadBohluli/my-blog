4b9f65c1820578cb1c76265296793bfcc75915e0
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-10-14 18:25:01 +0330

    implement refresh token controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,12 +4,14 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Req,
   Request,
   UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
+import { RefreshAuthGuard } from './guards/refresh-auth/refresh-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -28,5 +30,12 @@ export class AuthController {
     const refreshToken = this.authService.generateRefreshToken(req.user);
 
     return { accessToken, refreshToken, userId: req.user.userId };
+  }
+
+  @UseGuards(RefreshAuthGuard)
+  @Post('refresh')
+  refreshToken(@Req() req) {
+    const accessToken = this.authService.generateAccessToken(req.user);
+    return { accessToken };
   }
 }
