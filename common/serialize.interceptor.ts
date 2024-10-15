ea58f15a47508bace4baf4d3edeead79a0de18cc
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-10-16 01:26:16 +0330

    transform data with interceptor

--- common/serialize.interceptor.ts
+++ common/serialize.interceptor.ts
@@ -1,8 +1,16 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToClass, plainToInstance } from 'class-transformer';
 import { map } from 'rxjs';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(ctx: ExecutionContext, next: CallHandler) {
-    return next.handle().pipe(map((data: any) => {}));
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
   }
 }
