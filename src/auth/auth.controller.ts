657f0bb4e066f54bc61a2d47c38db48bb77de2ca
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-10-15 12:59:44 +0330

    implement current user decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,15 +4,14 @@ import {
   HttpCode,
   HttpStatus,
   Post,
-  Req,
   UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
 import { RefreshAuthGuard } from './guards/refresh-auth/refresh-auth.guard';
-import { Users } from '@prisma/client';
 
 @Controller('auth')
 export class AuthController {
@@ -26,27 +25,24 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  async login(@Req() req) {
-    const { accessToken, refreshToken } = await this.authService.login(
-      req.user,
-    );
+  async login(@CurrentUser() user) {
+    const { accessToken, refreshToken } = await this.authService.login(user);
 
-    return { accessToken, refreshToken, userId: req.user.userId };
+    return { accessToken, refreshToken, userId: user.userId };
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('signout')
-  async signout(@Req() req) {
-    await this.authService.signout(req.user.userId);
+  async signout(@CurrentUser() user) {
+    await this.authService.signout(user.userId);
     return { message: 'you are sign out successfuly' };
   }
 
   @UseGuards(RefreshAuthGuard)
   @Post('refresh')
-  async refreshToken(@Req() req) {
-    const { accessToken, refreshToken } = await this.authService.refreshToken(
-      req.user,
-    );
+  async refreshToken(@CurrentUser() user) {
+    const { accessToken, refreshToken } =
+      await this.authService.refreshToken(user);
 
     return { accessToken, refreshToken };
   }
