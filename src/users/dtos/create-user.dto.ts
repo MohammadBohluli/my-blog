bec06b4e3d48855d0ce832ceb097aa3294e5506a
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-10-16 18:34:53 +0330

    add api to create user dto

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,27 +1,52 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsOptional,
   IsString,
   MaxLength,
   MinLength,
+  NotContains,
 } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    required: true,
+    type: String,
+    uniqueItems: true,
+    example: 'example@gmail.com',
+  })
   @IsEmail()
   @IsString()
   email: string;
 
+  @ApiProperty({
+    required: true,
+    type: String,
+    minLength: 3,
+    maxLength: 30,
+    uniqueItems: true,
+    example: 'myUsername',
+  })
   @IsString()
   @MinLength(3)
   @MaxLength(30)
+  @NotContains(' ', { message: 'username should not contain a space' })
   username: string;
 
+  @ApiProperty({
+    required: false,
+    type: String,
+    minLength: 3,
+    maxLength: 30,
+    example: 'my name',
+  })
   @IsString()
   @IsOptional()
   @MinLength(3)
   @MaxLength(30)
   name: string;
 
+  @ApiProperty({ required: true, type: String, minLength: 3, maxLength: 12 })
   @IsString()
   @MinLength(8)
   @MaxLength(12)
