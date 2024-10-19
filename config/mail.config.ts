6be105f8af5a1a804761b014bb868d6f479298c2
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-10-19 21:49:52 +0330

    configure mail options

---
+++ config/mail.config.ts
@@ -0,0 +1,10 @@
+const mailConfig = () => ({
+  mailHost: process.env.MAIL_HOST,
+  mailPort: parseInt(process.env.MAIL_PORT),
+  mailSecure: Boolean(process.env.MAIL_SECURE),
+  mailUser: process.env.MAIL_USER,
+  mailPassword: process.env.MAIL_PASSWORD,
+  mailFrom: process.env.MAIL_FROM,
+});
+
+export default mailConfig;
--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { MailerModule } from '@nestjs-modules/mailer';
+import { MailerModule, MailerOptions } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MailerModule.forRoot()],
+  imports: [
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<MailerOptions> => ({
+        transport: {
+          host: configService.get<string>('mailHost'),
+          port: configService.get<number>('mailPort'),
+          secure: configService.get<boolean>('mailSecure'),
+          auth: {
+            user: configService.get<string>('mailUser'),
+            pass: configService.get<string>('mailPassword'),
+          },
+        },
+      }),
+    }),
+  ],
   providers: [MailService],
 })
 export class MailModule {}
