04be4b46a7c2d2a598c85b9a57a94e8f446d5533
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-10-19 17:53:24 +0330

    serialize user profile response

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
-import { UsersService } from './users.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
+import { Serializer } from 'src/common/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+import { UsersService } from './users.service';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
+  @Serializer(UserDto)
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Req() req) {
+  getProfile(@Req() req: Request) {
     return this.userService.findById(req.user.userId);
   }
 }
